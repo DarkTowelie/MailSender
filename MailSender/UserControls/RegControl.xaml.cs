8c24ba52c80cd775b91ae959c47253b55cda55dc
DarkTowelie <Valeriy.Shestakov@inbox.ru>
2023-05-14 11:52:06 +0700

    project created

---
+++ MailSender/UserControls/RegControl.xaml.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace MailSender.UserControls
+{
+    /// <summary>
+    /// Логика взаимодействия для RegControl.xaml
+    /// </summary>
+    public partial class RegControl : UserControl
+    {
+        public RegControl()
+        {
+            InitializeComponent();
+            cb_MailType.Items.Add("@mail.ru");
+            cb_MailType.Items.Add("@inbox.ru");
+            cb_MailType.SelectedIndex = 0;
+        }
+    }
+}
